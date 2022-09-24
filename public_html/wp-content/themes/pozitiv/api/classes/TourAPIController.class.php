56cf18eb9ed818f3658312c240aad8ad6cc84dbc
UNI-Studio <design@uni-studio.ru>
2022-09-24 20:32:04 +0300

    yb

---
+++ public_html/wp-content/themes/pozitiv/api/classes/TourAPIController.class.php
@@ -0,0 +1,10 @@
+<?php
+
+class TourAPIController extends WP_REST_Controller {
+
+    function __construct($request) {
+        var_dump($request);
+        // exit;
+    }
+
+}
--- public_html/wp-content/themes/pozitiv/functions.php
+++ public_html/wp-content/themes/pozitiv/functions.php
@@ -6,33 +6,33 @@ require_once __DIR__ . '/api/functions-api.php';    // Функции иници
 /* 
 *	Добавляем кастомные настройки для темы
 */
-add_action('customize_register', function($customizer) {
-    $customizer->add_section('custom_site_configs', array(
-        'title' => 'Настройки сайта',
-        'description' => 'Некоторые кастомные настройки',
-        'priority' => 11,
-    ));
-    $customizer->add_setting('phone-main-1', array('default' => ''));
-    $customizer->add_setting('phone-main-2', array('default' => ''));
-    $customizer->add_setting('email-contact', array('default' => ''));
+// add_action('customize_register', function($customizer) {
+//     $customizer->add_section('custom_site_configs', array(
+//         'title' => 'Настройки сайта',
+//         'description' => 'Некоторые кастомные настройки',
+//         'priority' => 11,
+//     ));
+//     $customizer->add_setting('phone-main-1', array('default' => ''));
+//     $customizer->add_setting('phone-main-2', array('default' => ''));
+//     $customizer->add_setting('email-contact', array('default' => ''));
 
 
-    $customizer->add_control('phone-main-1', array(
-            'label' => 'Основной телефон 1',
-            'section' => 'custom_site_configs',
-            'type' => 'text',)
-    );
-    $customizer->add_control('phone-main-2', array(
-            'label' => 'Основной телефон 2',
-            'section' => 'custom_site_configs',
-            'type' => 'text',)
-    );
-    $customizer->add_control('email-contact', array(
-            'label' => 'Основной e-mail',
-            'section' => 'custom_site_configs',
-            'type' => 'text',)
-    );
-});
+//     $customizer->add_control('phone-main-1', array(
+//             'label' => 'Основной телефон 1',
+//             'section' => 'custom_site_configs',
+//             'type' => 'text',)
+//     );
+//     $customizer->add_control('phone-main-2', array(
+//             'label' => 'Основной телефон 2',
+//             'section' => 'custom_site_configs',
+//             'type' => 'text',)
+//     );
+//     $customizer->add_control('email-contact', array(
+//             'label' => 'Основной e-mail',
+//             'section' => 'custom_site_configs',
+//             'type' => 'text',)
+//     );
+// });
 
 // Настроим ширину Gutenberg в Админке
 add_action( 'after_setup_theme', 'GutenbergCSS' ); 
@@ -48,37 +48,3 @@ function webp_upload_mimes( $existing_mimes ) {
     return $existing_mimes;
 }
 add_filter( 'mime_types', 'webp_upload_mimes' );
-
-
-// АКТИВАЦИЯ ВИДЖЕТОВ
-add_action( 'widgets_init', function() {
-    register_sidebar(
-        array(
-            'id' => 'sidebar_left_banners', // уникальный id
-            'name' => 'Баннеры в левой колонке', // название сайдбара
-            'before_widget' => '',
-            'after_widget' => '',
-            'before_title' => '',
-            'after_title' => ''
-        )
-    );
-});
-
-// Добавляем поддержку картинок для постов
-//add_theme_support('post-thumbnails', array('post')); // для всех типов постов
-
-
-
-// НАСТРОЙКА МЕНЮ
-add_theme_support('menus');
-// Регистрируем области меню
-register_nav_menus(
-    array(
-        'menu-main'   => 'Главное меню',
-        'menu-left-sidebar'   => 'Меню услуги',
-        'menu-adaptive'   => 'Меню адаптивного режима',
-    )
-);
-
-// Подключим файл с функциями шорткодов
-include 'functions-shortcodes.php';
