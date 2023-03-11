349ef4ee8212c81bc18eb145a7e6928929326c47
design@uni-studio.ru <design@uni-studio.ru>
2023-03-11 14:01:47 +0300

    Оптимизация кода

--- public_html/wp-content/themes/pozitiv/system/functions-pozitiv.php
+++ public_html/wp-content/themes/pozitiv/system/functions-pozitiv.php
@@ -1,161 +1,7 @@
 <?php
 
-/**
- *
- */
-
-// Добавим типы данных
-function cptui_register_my_cpts() {
-
-    $pluginName = 'pozitiv';
-
-	/**
-	 * Post Type: Туры.
-	 */
-	register_post_type(
-        'tour',
-        [
-            'label' => esc_html__( 'Туры', 'custom-post-type-ui' ),
-            'labels' => [
-                'name' => esc_html__( 'Туры', 'custom-post-type-ui' ),
-                'singular_name' => esc_html__( 'Тур', 'custom-post-type-ui' ),
-                'menu_name' => esc_html__( 'Туры', 'custom-post-type-ui' ),
-                'all_items' => esc_html__( 'Все туры', 'custom-post-type-ui' ),
-                'add_new' => esc_html__( 'Добавить новый', 'custom-post-type-ui' ),
-                'add_new_item' => esc_html__( 'Добавить новый тур', 'custom-post-type-ui' ),
-                'edit_item' => esc_html__( 'Изменить тур', 'custom-post-type-ui' ),
-                'new_item' => esc_html__( 'Новый тур', 'custom-post-type-ui' ),
-                'view_item' => esc_html__( 'Просмотр тура', 'custom-post-type-ui' ),
-                'view_items' => esc_html__( 'Просмотр туров', 'custom-post-type-ui' ),
-                'search_items' => esc_html__( 'Поиск туров', 'custom-post-type-ui' ),
-                'not_found' => esc_html__( 'не найдено', 'custom-post-type-ui' ),
-                'not_found_in_trash' => esc_html__( 'не найдено в корзине', 'custom-post-type-ui' ),
-            ],
-            'description' => '',
-            'public' => false,
-            'publicly_queryable' => false,
-            'show_ui' => true,
-            'show_in_rest' => true,
-            'rest_base' => 'tour',
-            // 'rest_controller_class' => 'WP_REST_Posts_Controller',
-            // 'rest_namespace' => "{$pluginName}/v1",
-            'has_archive' => false,
-            'show_in_menu' => true,
-            'show_in_nav_menus' => false,
-            'delete_with_user' => false,
-            'exclude_from_search' => false,
-            'capability_type' => 'post',
-            'map_meta_cap' => true,
-            'hierarchical' => false,
-            'can_export' => false,
-            'rewrite' => false,
-            'query_var' => true,
-            'menu_icon' => 'dashicons-palmtree',
-            'supports' => [ 'title', 'editor', 'thumbnail', 'page-attributes' ],
-            'taxonomies' => [ 'category' ],
-            'show_in_graphql' => false,
-        ]
-    );
-
-
-	/**
-	 * Post Type: Поездки.
-	 */
-	register_post_type(
-        'trip',
-        [
-            'label' => esc_html__( 'Поездки', 'custom-post-type-ui' ),
-            'labels' => [
-                'name' => esc_html__( 'Поездки', 'custom-post-type-ui' ),
-                'singular_name' => esc_html__( 'Поездка', 'custom-post-type-ui' ),
-                'menu_name' => esc_html__( 'Поездки', 'custom-post-type-ui' ),
-                'all_items' => esc_html__( 'Все поездки', 'custom-post-type-ui' ),
-                'add_new' => esc_html__( 'Добавить новую', 'custom-post-type-ui' ),
-                'add_new_item' => esc_html__( 'Добавить новую поездку', 'custom-post-type-ui' ),
-                'edit_item' => esc_html__( 'Изменить поездку', 'custom-post-type-ui' ),
-                'new_item' => esc_html__( 'Новая поездка', 'custom-post-type-ui' ),
-                'view_item' => esc_html__( 'Просмотр поездки', 'custom-post-type-ui' ),
-                'view_items' => esc_html__( 'Просмотр поездок', 'custom-post-type-ui' ),
-                'search_items' => esc_html__( 'Поиск поездок', 'custom-post-type-ui' ),
-                'not_found' => esc_html__( 'не найдено', 'custom-post-type-ui' ),
-                'not_found_in_trash' => esc_html__( 'не найдено в корзине', 'custom-post-type-ui' ),
-            ],
-            'description' => '',
-            'public' => false,
-            'publicly_queryable' => false,
-            'show_ui' => true,
-            'show_in_rest' => true,
-            'rest_base' => 'trip',
-            'rest_controller_class' => 'WP_REST_Posts_Controller',
-            'rest_namespace' => "{$pluginName}/v1",
-            'has_archive' => false,
-            'show_in_menu' => true,
-            'show_in_nav_menus' => false,
-            'delete_with_user' => false,
-            'exclude_from_search' => false,
-            'capability_type' => 'post',
-            'map_meta_cap' => true,
-            'hierarchical' => false,
-            'can_export' => false,
-            'rewrite' => false,
-            'query_var' => true,
-            'menu_icon' => 'dashicons-palmtree',
-            'supports' => [ 'title', 'editor', 'thumbnail' ],
-            'taxonomies' => [],
-            'show_in_graphql' => false,
-        ]
-    );
-
-
-	/**
-	 * Post Type: Допуслуга.
-	 */
-	register_post_type(
-        'service',
-        [
-            'label' => esc_html__( 'Услуги', 'custom-post-type-ui' ),
-            'labels' => [
-                'name' => esc_html__( 'Услуги', 'custom-post-type-ui' ),
-                'singular_name' => esc_html__( 'Услуга', 'custom-post-type-ui' ),
-                'menu_name' => esc_html__( 'Услуги', 'custom-post-type-ui' ),
-                'all_items' => esc_html__( 'Все услуги', 'custom-post-type-ui' ),
-                'add_new' => esc_html__( 'Добавить новую', 'custom-post-type-ui' ),
-                'add_new_item' => esc_html__( 'Добавить новую услугу', 'custom-post-type-ui' ),
-                'edit_item' => esc_html__( 'Изменить услугу', 'custom-post-type-ui' ),
-                'new_item' => esc_html__( 'Новая услуга', 'custom-post-type-ui' ),
-                'view_item' => esc_html__( 'Просмотр услуги', 'custom-post-type-ui' ),
-                'view_items' => esc_html__( 'Просмотр услуг', 'custom-post-type-ui' ),
-                'search_items' => esc_html__( 'Поиск услуг', 'custom-post-type-ui' ),
-                'not_found' => esc_html__( 'не найдено', 'custom-post-type-ui' ),
-                'not_found_in_trash' => esc_html__( 'не найдено в корзине', 'custom-post-type-ui' ),
-            ],
-            'description' => '',
-            'public' => false,
-            'publicly_queryable' => false,
-            'show_ui' => true,
-            'show_in_rest' => true,
-            'rest_base' => 'service',
-            'rest_controller_class' => 'WP_REST_Posts_Controller',
-            'rest_namespace' => "{$pluginName}/v1",
-            'has_archive' => false,
-            'show_in_menu' => true,
-            'show_in_nav_menus' => false,
-            'delete_with_user' => false,
-            'exclude_from_search' => false,
-            'capability_type' => 'post',
-            'map_meta_cap' => true,
-            'hierarchical' => false,
-            'can_export' => false,
-            'rewrite' => false,
-            'query_var' => true,
-            'menu_icon' => 'dashicons-palmtree',
-            'supports' => [ 'title', 'editor'],
-            'taxonomies' => ['category'],
-            'show_in_graphql' => false,
-        ]
-    );
-}
-add_action( 'init', 'cptui_register_my_cpts' );
+require_once 'functions-pozitiv-hooks.php';
+require_once 'functions-pozitiv-types.php';
 
 // Добавление роли для пользователей позитива
 // $result = add_role(
@@ -238,7 +84,7 @@ add_action( 'rest_api_init', function(){
     // Маршрут Платеж
     register_rest_route(
         POZITIV_API_NAMESPACE,
-        '/payment/(?P<action>[a-z]+)/',
+        '/payment/(?P<action>[a-zA-Z]+)/',
         [
             'methods'   => 'POST',
             'callback'  => 'EndpointPayment',
@@ -251,74 +97,75 @@ add_action( 'rest_api_init', function(){
                 ]
             ],
             'permission_callback' => function ( $request ) {
-
+                return true;
             }
         ]
     );
 
+    {
+        // Маршрут Поездка
+        // register_rest_route(
+        //     POZITIV_API_NAMESPACE,
+        //     'trip/(?P<method>[a-z]+)/',
+        //     [
+        //         'methods'   => 'POST',
+        //         'callback'  => 'EndpointTrip',
+        //         'args'      => [
+        //             'paramName'     =>  [
+        //                 'default'               => null,            // значение параметра по умолчанию
+        //                 'required'              => null,            // является ли параметр обязательным. Может быть только true
+        //                 'validate_callback'     => 'function_name', // функция проверки значения параметра. Должна вернуть true/false
+        //                 'sanitize_callback'     => 'function_name', // функция очистки значения параметра. Должна вернуть очищенное значение
+        //             ]
+        //         ],
+        //         'permission_callback' => function ( $request ) {
 
-    // Маршрут Поездка
-    // register_rest_route(
-    //     POZITIV_API_NAMESPACE,
-    //     'trip/(?P<method>[a-z]+)/',
-    //     [
-    //         'methods'   => 'POST',
-    //         'callback'  => 'EndpointTrip',
-    //         'args'      => [
-    //             'paramName'     =>  [
-    //                 'default'               => null,            // значение параметра по умолчанию
-    //                 'required'              => null,            // является ли параметр обязательным. Может быть только true
-    //                 'validate_callback'     => 'function_name', // функция проверки значения параметра. Должна вернуть true/false
-    //                 'sanitize_callback'     => 'function_name', // функция очистки значения параметра. Должна вернуть очищенное значение
-    //             ]
-    //         ],
-    //         'permission_callback' => function ( $request ) {
+        //         }
+        //     ]
+        // );
 
-    //         }
-    //     ]
-    // );
+        // Маршрут Заказ
+        // register_rest_route(
+        //     POZITIV_API_NAMESPACE,
+        //     'order/(?P<method>[a-z]+)/',
+        //     [
+        //         'methods'   => 'POST',
+        //         'callback'  => 'EndpointOrder',
+        //         'args'      => [
+        //             'paramName'     =>  [
+        //                 'default'               => null,            // значение параметра по умолчанию
+        //                 'required'              => null,            // является ли параметр обязательным. Может быть только true
+        //                 'validate_callback'     => 'function_name', // функция проверки значения параметра. Должна вернуть true/false
+        //                 'sanitize_callback'     => 'function_name', // функция очистки значения параметра. Должна вернуть очищенное значение
+        //             ]
+        //         ],
+        //         'permission_callback' => function ( $request ) {
 
-    // Маршрут Заказ
-    // register_rest_route(
-    //     POZITIV_API_NAMESPACE,
-    //     'order/(?P<method>[a-z]+)/',
-    //     [
-    //         'methods'   => 'POST',
-    //         'callback'  => 'EndpointOrder',
-    //         'args'      => [
-    //             'paramName'     =>  [
-    //                 'default'               => null,            // значение параметра по умолчанию
-    //                 'required'              => null,            // является ли параметр обязательным. Может быть только true
-    //                 'validate_callback'     => 'function_name', // функция проверки значения параметра. Должна вернуть true/false
-    //                 'sanitize_callback'     => 'function_name', // функция очистки значения параметра. Должна вернуть очищенное значение
-    //             ]
-    //         ],
-    //         'permission_callback' => function ( $request ) {
+        //         }
+        //     ]
+        // );
 
-    //         }
-    //     ]
-    // );
+        // Маршрут Допуслуга
+        // register_rest_route(
+        //     POZITIV_API_NAMESPACE,
+        //     'service/(?P<method>[a-z]+)/',
+        //     [
+        //         'methods'   => 'POST',
+        //         'callback'  => 'EndpointService',
+        //         'args'      => [
+        //             'paramName'     =>  [
+        //                 'default'               => null,            // значение параметра по умолчанию
+        //                 'required'              => null,            // является ли параметр обязательным. Может быть только true
+        //                 'validate_callback'     => 'function_name', // функция проверки значения параметра. Должна вернуть true/false
+        //                 'sanitize_callback'     => 'function_name', // функция очистки значения параметра. Должна вернуть очищенное значение
+        //             ]
+        //         ],
+        //         'permission_callback' => function ( $request ) {
 
-    // Маршрут Допуслуга
-    // register_rest_route(
-    //     POZITIV_API_NAMESPACE,
-    //     'service/(?P<method>[a-z]+)/',
-    //     [
-    //         'methods'   => 'POST',
-    //         'callback'  => 'EndpointService',
-    //         'args'      => [
-    //             'paramName'     =>  [
-    //                 'default'               => null,            // значение параметра по умолчанию
-    //                 'required'              => null,            // является ли параметр обязательным. Может быть только true
-    //                 'validate_callback'     => 'function_name', // функция проверки значения параметра. Должна вернуть true/false
-    //                 'sanitize_callback'     => 'function_name', // функция очистки значения параметра. Должна вернуть очищенное значение
-    //             ]
-    //         ],
-    //         'permission_callback' => function ( $request ) {
-
-    //         }
-    //     ]
-    // );
+        //         }
+        //     ]
+        // );
+    }
 } );
 
 
@@ -354,32 +201,8 @@ add_filter( 'rest_authentication_errors', function($errors) {
 });
 
 
-// Реакция на сохранение поста
-add_action(
-    'save_post',
-    function ($postID, $post, $update) {
-        if ($post->post_type == 'tour') {
-            
-            $existSlug = get_field('slug');
-            if (empty($existSlug)) {
-                $slug = wp_unique_post_slug($post->post_title, $post->ID, $post->post_status, 'tour', 0);
-            } else {
-                $slug = $existSlug;
-            }
-            
-            update_field('slug', $slug, $post->ID);
-        }
 
-        return true;
-    }, 
-    10, 
-    3 
-);
-
-
-/**
- * Добавим меню Заказы в админку
- */
+// Добавим меню Заказы в админку
 add_action(
     'admin_menu',
     function(){
@@ -405,13 +228,11 @@ add_action(
     25
 );
 
-
 function PozitivOrderPage(){
     require_once __DIR__ . '/models/orderModel.class.php';
     require_once __DIR__ . '/pagesAdmin/ordersPageAdmin.class.php';
 
     $orderModel = new OrderModel(); 
-
 
     switch ($_REQUEST['action']) {
         case 'edit':    // `показать страницу редактирования заказа
